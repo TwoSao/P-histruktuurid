cfb22cd1452b52f5ae9d24e26ad1e2f4d0c0f28a
opilane <opilane@HP-CZC2349HTF>
2025-01-20 11:28:25 +0200

    msg

--- Põhistruktuurid/Põhistruktuurid.py
+++ Põhistruktuurid/Põhistruktuurid.py
@@ -1,1 +1,15 @@
+from string import printable
 
+
+print("Tere!")
+a=5
+aste=2
+tulemus=a**aste
+print(f"{a}**{aste} =",tulemus)
+
+b=int(input())
+c=int(input())
+
+print(f"{b}/{c}={a/c}")
+print(f"{b}-{c}={a-c}")
+print(f"{b}+{c}={a+c}")
